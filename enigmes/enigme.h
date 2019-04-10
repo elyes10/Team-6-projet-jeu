12000d02997e2cf1052659ac7ea2f355a663e982
elyes10 <47626987+elyes10@users.noreply.github.com>
2019-04-10 22:33:17 +0100

    Update enigme.h

--- enigmes/enigme.h
+++ enigmes/enigme.h
@@ -9,6 +9,6 @@ typedef struct
 void init_enigme(enigme * e);
 void generate_afficher (SDL_Surface * screen  , char image [],enigme *e,int *alea);
 int solution_e (char image []);
-void resolution (int * running,int *run,int *r );
+void resolution (int *r );
 void afficher_resultat (SDL_Surface * screen,int solution,int r,enigme *en);
 #endif // ENIGME_H_INCLUDED
